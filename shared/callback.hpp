5e474c2c75fa2301fe9d66692ed623fc343b6988
Kyle Smith Hanna <kylesmithhanna@gmail.com>
2022-07-08 12:50:22 -0500

    Add score buffer for indentification

--- shared/callback.hpp
+++ shared/callback.hpp
@@ -2,7 +2,10 @@
 
 #include "beatsaber-hook/shared/utils/typedefs-wrappers.hpp"
 
+#include "GlobalNamespace/CutScoreBuffer.hpp"
+
 struct SwingInfo {
+    GlobalNamespace::CutScoreBuffer* scoreBuffer;
     bool leftSaber;
     int preSwing = 0;
     int postSwing = 0;
